d9582bbc2653b667234a694c3d47c84663553d5f
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2023-03-01 16:15:44 +0300

    Update obo-for-linux.py

--- OBO-PY/obo-for-linux.py
+++ OBO-PY/obo-for-linux.py
@@ -9,7 +9,7 @@ def ekranTemizle():
 def baslangic():
     ekranTemizle()
 
-    print("OBO PY Edition 0.1 Beta - https://github.com/furcanomer/obo-py\n--------------------------------------------------------------------------------")
+    print("OBO PY Edition 0.2 Beta - https://github.com/furcanomer/obo-py\n--------------------------------------------------------------------------------")
     print("Saga gitmek icin 'D' tusuna basin.\tYeniden baslamak icin 'R' tusuna basin.\n")
     print("Sola gitmek icin 'A' tusuna basin.\tOyunu kapatmak icin 'X' tusuna basin")
     print("--------------------------------------------------------------------------------\n")
@@ -17,6 +17,9 @@ def baslangic():
 def yeniden_Baslangic():
     global ekran_silme
     ekran_silme = 1
+
+def birsey_yapma():
+    print("\a", end='')
 
 
 baslangic()
@@ -38,7 +41,7 @@ while True:
             adim += 1
 
         else:
-            print("\a", end='')
+            birsey_yapma()
 
     elif tus == 'a' or tus == 'A':
 
@@ -54,7 +57,7 @@ while True:
             adim = 0
 
         elif adim == 0:
-            print("\a", end='')
+            birsey_yapma()
             continue
 
     elif tus == 'x' or tus == 'X':
