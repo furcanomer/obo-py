d9a6916345f47190c384271e30784aea14dd8b78
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2023-03-01 20:07:34 +0300

    Update obo.py

--- OBO-PY/obo.py
+++ OBO-PY/obo.py
@@ -14,6 +14,10 @@ def ekranTemizle():
 
 def baslangic():
     ekranTemizle()
+    """
+    Eğer dosya konsoldan açıldıysa konsolla alakalı yazıların ekrandan silinmeisini sağlar.
+	Ekranın sadece oyun ile alakalı olmasını istiyorum.
+    """
 
     print("OBO PY Edition 1.0.1 - https://github.com/furcanomer/obo-py\n--------------------------------------------------------------------------------")
     print("Saga gitmek icin 'D' tusuna basin.\tYeniden baslamak icin 'R' tusuna basin.\n")
@@ -30,6 +34,12 @@ def birsey_yapma():
 baslangic()
 
 adim = 0
+"""
+adim değişkeni sayseinde kaç adet sağa gittiğimi ölçüyorum ki programa sola git dediğimde sağa gittiğim kadar da sola gitsin.
+ekran_silme değişkenini sıfır yaptım ki ekran silme koşulu çalışmasın.
+oyuncu oyuna baştan başlamak istediği zaman ekran_silme = 1 oluyor ve koşul sağlanıyor ve hemen ardından geri 0 oluyor.	
+		
+"""
 
 while True:
     if ekran_silme == 1:
@@ -38,36 +48,48 @@ while True:
         adim = 0
 
     tus = readchar.readkey()
+    """
+    readkey() ile istedim ki ekrana D harfi basılmasın.
+    """
 
     if tus == 'd' or tus == 'D':
         
-        if adim < 19:
+        if adim < 19: # ok sayısını 19 ile sınırlayan kod
             print("--> ", end='', flush=True)
             """
             ekrana "-->" stringini bas ama varsayılan olarak satır atlama
             yani boş karakter bas ve ekrana ne basacaksan anında bas (flush=True)
             """
-            adim += 1
+            adim += 1 # her sağa gittiğimizde adim 1 artıyor
 
         else:
             birsey_yapma()
+            # ok sınırıda rağmen hala sağa gidilmeye çalışılıyorsa sistem uyarı sesi verecek
 
     elif tus == 'a' or tus == 'A':
 
-        if adim > 0:
+        if adim > 0: # eğer sağa gittiysek
 
             print("\n<--", end = '', flush=True)
 
             for i in range(adim,1,-1):
+                """
+                for döngüsü sayesinde sağa gittiğimiz adim sayisi kadar
+				sola gidebiliyoruz.
+				"""
                 print(" <--", end='')
             
-            print("\n")
+            print("\n") # sola gittikten sonra 2 satır aşağı atlıyoruz.
 
             adim = 0
+            """
+            Sola gittikten sonra adimi sıfırlıyoruz
+			Sıfırlamazsak sola gitme tuşuna bastığımızda, eski sağ adim sayisi kadar sola gideriz.
+			"""
 
-        elif adim == 0:
-            birsey_yapma()
-            continue
+        elif adim == 0: # // sola gitme tuşuna bastık evet ama eğer hiç sağa gitmemişsek...?
+            birsey_yapma() # ...o zaman hiçbir şey yapmıyoruz ve sistem uyarı sesi çıkarıp
+            continue # while döngüsünün başına dönüyoruz.
 
     elif tus == 'x' or tus == 'X':
         ekranTemizle()
@@ -78,4 +100,4 @@ while True:
         continue
 
     else:
-        continue
+        continue # A - D  tuşlarına basılmadıysa hiçbirşey yapılmıyor ve while döngüsünün başına dönüyoruz
